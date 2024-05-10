c4f02e7b304bec228d81c75d30f3c6dfec218dd3
buru93 <aburruezotorner@gmail.com>
2024-05-10 21:17:25 +0200

    Preview Llibret v1.0

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    // simulateTouch: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
